9a7dc6688622ea3939968009469e8da0d013f314
Jan Lepper <jan_lepper@gmx.de>
2023-12-14 22:46:21 +0100

    avoid including system headers - remove implementations of plog::util:: ftime(), localtime_s(), gettid(), processFuncName(), gmtime_s() - it is left for client code to implement these

--- include/plog/Util.h
+++ include/plog/Util.h
@@ -3,24 +3,7 @@
 #include <cstring>
 #include <cstdio>
 #include <sstream>
-#include <fcntl.h>
-#include <sys/stat.h>
-
-#ifdef _WIN32
-#   include <plog/WinApi.h>
-#   include <time.h>
-#   include <sys/timeb.h>
-#   include <io.h>
-#   include <share.h>
-#elif defined(__rtems__)
-#   include <unistd.h>
-#   include <rtems.h>
-#else
-#   include <unistd.h>
-#   include <sys/syscall.h>
-#   include <sys/time.h>
-#   include <pthread.h>
-#endif
+#include <time.h>
 
 #define PLOG_NSTR(x)    x
 
@@ -33,121 +16,20 @@ namespace plog
         typedef std::istringstream nistringstream;
         typedef char nchar;
 
-        inline void localtime_s(struct tm* t, const time_t* time)
-        {
-#if defined(_WIN32) && defined(__BORLANDC__)
-            ::localtime_s(time, t);
-#elif defined(_WIN32) && defined(__MINGW32__) && !defined(__MINGW64_VERSION_MAJOR)
-            *t = *::localtime(time);
-#elif defined(_WIN32)
-            ::localtime_s(t, time);
-#else
-            ::localtime_r(time, t);
-#endif
-        }
 
-        inline void gmtime_s(struct tm* t, const time_t* time)
-        {
-#if defined(_WIN32) && defined(__BORLANDC__)
-            ::gmtime_s(time, t);
-#elif defined(_WIN32) && defined(__MINGW32__) && !defined(__MINGW64_VERSION_MAJOR)
-            *t = *::gmtime(time);
-#elif defined(_WIN32)
-            ::gmtime_s(t, time);
-#else
-            ::gmtime_r(time, t);
-#endif
-        }
-
-#ifdef _WIN32
-        typedef timeb Time;
-
-        inline void ftime(Time* t)
-        {
-            ::ftime(t);
-        }
-#else
         struct Time
         {
             time_t time;
             unsigned short millitm;
         };
 
-        inline void ftime(Time* t)
-        {
-            timeval tv;
-            ::gettimeofday(&tv, NULL);
 
-            t->time = tv.tv_sec;
-            t->millitm = static_cast<unsigned short>(tv.tv_usec / 1000);
-        }
-#endif
+        void ftime(Time* t);
+        void localtime_s(struct tm* t, const time_t* time);
+        unsigned int gettid();
+        std::string processFuncName(const char* func);
+        void gmtime_s(struct tm* t, const time_t* time);
 
-        inline unsigned int gettid()
-        {
-#ifdef _WIN32
-            return GetCurrentThreadId();
-#elif defined(__linux__)
-            return static_cast<unsigned int>(::syscall(__NR_gettid));
-#elif defined(__FreeBSD__)
-            long tid;
-            syscall(SYS_thr_self, &tid);
-            return static_cast<unsigned int>(tid);
-#elif defined(__rtems__)
-            return rtems_task_self();
-#elif defined(__APPLE__)
-            uint64_t tid64;
-            pthread_threadid_np(NULL, &tid64);
-            return static_cast<unsigned int>(tid64);
-#endif
-        }
-
-        inline std::string processFuncName(const char* func)
-        {
-#if (defined(_WIN32) && !defined(__MINGW32__)) || defined(__OBJC__)
-            return std::string(func);
-#else
-            const char* funcBegin = func;
-            const char* funcEnd = ::strchr(funcBegin, '(');
-
-            if (!funcEnd)
-            {
-                return std::string(func);
-            }
-
-            for (const char* i = funcEnd - 1; i >= funcBegin; --i) // search backwards for the first space char
-            {
-                if (*i == ' ')
-                {
-                    funcBegin = i + 1;
-                    break;
-                }
-            }
-
-            return std::string(funcBegin, funcEnd);
-#endif
-        }
-
-        inline const nchar* findExtensionDot(const nchar* fileName)
-        {
-            return std::strrchr(fileName, '.');
-        }
-
-        inline void splitFileName(const nchar* fileName, nstring& fileNameNoExt, nstring& fileExt)
-        {
-            const nchar* dot = findExtensionDot(fileName);
-
-            if (dot)
-            {
-                fileNameNoExt.assign(fileName, dot);
-                fileExt.assign(dot + 1);
-            }
-            else
-            {
-                fileNameNoExt.assign(fileName);
-                fileExt.clear();
-            }
-        }
 
         class NonCopyable
         {
@@ -161,83 +43,6 @@ namespace plog
             NonCopyable& operator=(const NonCopyable&);
         };
 
-        class Mutex : NonCopyable
-        {
-        public:
-            Mutex()
-            {
-#ifdef _WIN32
-                InitializeCriticalSection(&m_sync);
-#elif defined(__rtems__)
-                rtems_semaphore_create(0, 1,
-                            RTEMS_PRIORITY |
-                            RTEMS_BINARY_SEMAPHORE |
-                            RTEMS_INHERIT_PRIORITY, 1, &m_sync);
-#else
-                ::pthread_mutex_init(&m_sync, 0);
-#endif
-            }
-
-            ~Mutex()
-            {
-#ifdef _WIN32
-                DeleteCriticalSection(&m_sync);
-#elif defined(__rtems__)
-                rtems_semaphore_delete(m_sync);
-#else
-                ::pthread_mutex_destroy(&m_sync);
-#endif
-            }
-
-            friend class MutexLock;
-
-        private:
-            void lock()
-            {
-#ifdef _WIN32
-                EnterCriticalSection(&m_sync);
-#elif defined(__rtems__)
-                rtems_semaphore_obtain(m_sync, RTEMS_WAIT, RTEMS_NO_TIMEOUT);
-#else
-                ::pthread_mutex_lock(&m_sync);
-#endif
-            }
-
-            void unlock()
-            {
-#ifdef _WIN32
-                LeaveCriticalSection(&m_sync);
-#elif defined(__rtems__)
-                rtems_semaphore_release(m_sync);
-#else
-                ::pthread_mutex_unlock(&m_sync);
-#endif
-            }
-
-        private:
-#ifdef _WIN32
-            CRITICAL_SECTION m_sync;
-#else
-            pthread_mutex_t m_sync;
-#endif
-        };
-
-        class MutexLock : NonCopyable
-        {
-        public:
-            MutexLock(Mutex& mutex) : m_mutex(mutex)
-            {
-                m_mutex.lock();
-            }
-
-            ~MutexLock()
-            {
-                m_mutex.unlock();
-            }
-
-        private:
-            Mutex& m_mutex;
-        };
 
         template<class T>
         class Singleton : NonCopyable
